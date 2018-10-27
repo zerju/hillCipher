9f20f7468d4ce5975fdcfb36dc238652f523e41d
zerju <jure.zerak@gmail.com>
2018-10-27 11:42:05 +0200

    some commit

---
+++ HillCypher/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace HillCypher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.encrypt = new System.Windows.Forms.Button();
+            this.decrypt = new System.Windows.Forms.Button();
+            this.matrixKeyText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileBtn = new System.Windows.Forms.Button();
+            this.fileText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // encrypt
+            // 
+            this.encrypt.Location = new System.Drawing.Point(12, 208);
+            this.encrypt.Name = "encrypt";
+            this.encrypt.Size = new System.Drawing.Size(75, 23);
+            this.encrypt.TabIndex = 0;
+            this.encrypt.Text = "encrypt";
+            this.encrypt.UseVisualStyleBackColor = true;
+            this.encrypt.Click += new System.EventHandler(this.encrypt_Click);
+            // 
+            // decrypt
+            // 
+            this.decrypt.Location = new System.Drawing.Point(184, 208);
+            this.decrypt.Name = "decrypt";
+            this.decrypt.Size = new System.Drawing.Size(75, 23);
+            this.decrypt.TabIndex = 1;
+            this.decrypt.Text = "decrypt";
+            this.decrypt.UseVisualStyleBackColor = true;
+            this.decrypt.Click += new System.EventHandler(this.decrypt_Click);
+            // 
+            // matrixKeyText
+            // 
+            this.matrixKeyText.Location = new System.Drawing.Point(97, 21);
+            this.matrixKeyText.Name = "matrixKeyText";
+            this.matrixKeyText.Size = new System.Drawing.Size(148, 20);
+            this.matrixKeyText.TabIndex = 2;
+            this.matrixKeyText.TextChanged += new System.EventHandler(this.matrixKeyText_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Matrix Key";
+            // 
+            // openFileBtn
+            // 
+            this.openFileBtn.Location = new System.Drawing.Point(12, 56);
+            this.openFileBtn.Name = "openFileBtn";
+            this.openFileBtn.Size = new System.Drawing.Size(75, 23);
+            this.openFileBtn.TabIndex = 4;
+            this.openFileBtn.Text = "Open file...";
+            this.openFileBtn.UseVisualStyleBackColor = true;
+            this.openFileBtn.Click += new System.EventHandler(this.openFileBtn_Click);
+            // 
+            // fileText
+            // 
+            this.fileText.Location = new System.Drawing.Point(15, 85);
+            this.fileText.Multiline = true;
+            this.fileText.Name = "fileText";
+            this.fileText.ReadOnly = true;
+            this.fileText.Size = new System.Drawing.Size(244, 117);
+            this.fileText.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.fileText);
+            this.Controls.Add(this.openFileBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.matrixKeyText);
+            this.Controls.Add(this.decrypt);
+            this.Controls.Add(this.encrypt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button encrypt;
+        private System.Windows.Forms.Button decrypt;
+        private System.Windows.Forms.TextBox matrixKeyText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button openFileBtn;
+        private System.Windows.Forms.TextBox fileText;
+    }
+}
+
---
+++ HillCypher/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HillCypher
+{
+    public partial class Form1 : Form
+    {
+
+        string textInFile;
+        string alphabet = "ABCČDEFGHIJKLMNOPRSŠTUVZŽ".ToLower();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Text Files|*.txt";
+            openFileDialog1.Title = "Select a text File";
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string filename = openFileDialog1.FileName;
+                textInFile = System.IO.File.ReadAllText(@filename);
+                fileText.Text = textInFile;
+            }
+        }
+
+        private void encrypt_Click(object sender, EventArgs e)
+        {
+            string encText = encryptText(textInFile.ToLower());
+            fileText.Text = encText;
+            System.IO.File.WriteAllText(@Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\hillEncrypted.txt", encText);
+        }
+
+        private void decrypt_Click(object sender, EventArgs e)
+        {
+            string decText = decryptText(textInFile.ToLower());
+            fileText.Text = decText;
+            System.IO.File.WriteAllText(@Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\hillDecrypted.txt", decText);
+        }
+
+        private string encryptText(string textToEncrypt)
+        {
+            int[,] matrix = getMatrixFromText(matrixKeyText.Text);
+            string text = cleanText(textToEncrypt);
+            string newText = "";
+            for(int i = 0;i < text.Length; i = i + 2) {
+                int numOne = alphabet.IndexOf(text[i]);
+                int numTwo = alphabet.IndexOf(text[i + 1]);
+                int firstProduct = matrix[0, 0] * numOne + matrix[0, 1] * numTwo;
+                int secondProduct = matrix[1, 0] * numOne + matrix[1, 1] * numTwo;
+                newText = newText + alphabet[firstProduct % 25];
+                newText = newText + alphabet[secondProduct % 25];
+            }
+            return newText;
+        }
+
+
+        private string decryptText(string textToDecrypt)
+        {
+            int[,] inputMatrix = getMatrixFromText(matrixKeyText.Text);
+            string text = textToDecrypt;
+            string newText = "";
+            double [,] matrix = inverseMatrix(inputMatrix);
+            for (int i = 0; i < text.Length; i = i + 2)
+            {
+                int numOne = alphabet.IndexOf(text[i]);
+                int numTwo = alphabet.IndexOf(text[i + 1]);
+                double firstProduct = matrix[0, 0] * numOne + matrix[0, 1] * numTwo;
+                double secondProduct = matrix[1, 0] * numOne + matrix[1, 1] * numTwo;
+                int firstRounded = Convert.ToInt32(Math.Floor(firstProduct) % 25);
+                int secondRounded = Convert.ToInt32(Math.Floor(secondProduct) % 25);
+              
+                newText = newText + alphabet[firstRounded];
+                newText = newText + alphabet[secondRounded];
+            }
+
+            return newText;
+        }
+
+        private double[,] inverseMatrix(int[,] matrix)
+        {
+            double[,] inverse = new double[2, 2];
+
+            int determinant = matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
+            if(determinant < 0)
+            {
+                determinant = determinant * -1;
+            }
+            double detFraction = modInverse(determinant, 25);
+            inverse[0, 0] = (matrix[1, 1] * detFraction)%25;
+            inverse[0, 1] = ((-matrix[0, 1] + 25) * detFraction) % 25;
+            inverse[1, 0] = ((-matrix[1, 0] + 25) * detFraction) % 25;
+            inverse[1, 1] = (matrix[0, 0] * detFraction) % 25;
+
+            return inverse;
+        }
+
+        int modInverse(int a, int n)
+        {
+            int i = n, v = 0, d = 1;
+            while (a > 0)
+            {
+                int t = i / a, x = a;
+                a = i % x;
+                i = x;
+                x = d;
+                d = v - t * x;
+                v = x;
+            }
+            v %= n;
+            if (v < 0) v = (v + n) % n;
+            return v;
+        }
+
+        private int[,] getMatrixFromText(string text)
+        {
+            int[,] matrix = new int[2, 2];
+            int textCounter = 0;
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    matrix[i, j] = alphabet.IndexOf(text[textCounter]);
+                    textCounter++;
+                }
+            }
+            return matrix;
+        }
+
+        private string cleanText(string text)
+        {
+            string cleanedText = "";
+            for(int i = 0; i < text.Length; i++)
+            {
+                if(alphabet.IndexOf(text[i]) > -1)
+                {
+                    cleanedText += text[i];
+                }
+            }
+            if(cleanedText.Length % 2 > 0)
+            {
+                cleanedText = cleanedText + "ž";
+            }
+            return cleanedText;
+        }
+        
+
+
+        private void matrixKeyText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
